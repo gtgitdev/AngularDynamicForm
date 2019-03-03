2410d8a434f4c302b66f95e59f92f4e7502165fd
gtgitdev <g.turton@hotmail.com>
2019-03-03 14:13:21 -0500

    form binding

--- dynamicclient/src/app/dynamicform/dynamicform.module.ts
+++ dynamicclient/src/app/dynamicform/dynamicform.module.ts
@@ -5,6 +5,7 @@ import { MaterialModule } from '../shared/material.module';
 import { DynamicformRoutingModule } from './dynamicform-routing.module';
 import { DynamicFormFieldComponent } from './dynamic-form-field/dynamic-form-field.component';
 import { DynamicFormComponent } from './dynamic-form/dynamic-form.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { DynamicFormComponent } from './dynamic-form/dynamic-form.component';
     CommonModule,
     DynamicformRoutingModule,
     MaterialModule,
+    FormsModule,
   ]
 })
 export class DynamicformModule { }
